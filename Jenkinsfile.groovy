378c55bfa0ebd5186571bfeeb691e826516081bb
Bogdan Tarabrin <tarabrin@ubrr.ru>
2021-03-29 18:08:12 +0300

    + Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,16 @@
+def packageName   = 'ubrr_integr'
+def registryName  = 'nexus.dev.ubrr.ru'
+def imageFullName = ''
+pipeline {
+    options {
+        gitLabConnection('OTP Gitlab')
+        buildDiscarder logRotator(numToKeepStr: '5')
+    }
     post {
         cleanup {
             script {
-                if (env.imageFullName) {
-                    sh "docker rmi $imageFullName"
+                if (imageFullName) {
+                    sh "docker rmi " + imageFullName
                 }
             }
             cleanWs()
@@ -43,3 +51,39 @@
                 to: '$DEFAULT_RECIPIENTS',
                 attachLog: false
         }
+    }
+    agent any
+    stages {
+        stage('Prepare parameters') {
+            steps {
+                gitlabCommitStatus(STAGE_NAME) {
+                    echo "Opa!"
+                }
+            }
+        }
+        stage('Compile project') {
+            agent {
+                docker { 
+                    image registryName + '/dockerhub/maven:3.3-jdk-8'
+                    reuseNode true
+                    args '-v ${workspace}:/build -v /etc/hosts:/etc/hosts:ro'
+                }
+            }
+            steps {
+                gitlabCommitStatus(STAGE_NAME) {
+                    sh "cd /build"
+                    sh "mvn clean install"
+                }
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                gitlabCommitStatus(STAGE_NAME) {
+                        imageFullName = registryName + '/' + packageName + ":0.0.1"
+                        dockerImage = docker.build(imageFullName)
+                    }
+                }
+            }
+        }
+    }
+}
